14e701a67c3a2d4e29f57b9aace2324bc50bb38a
Guilherme <124900399+guiFrcs@users.noreply.github.com>
2026-06-12 21:13:13 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
         OLHA QUE HABILIDADE 😏🤠🧐 OLHA QUE HABILIDADE 😏🤠🧐
         EU QUERO UMA SALADA DE FRUTA, JAMES 😉🍏🍇.  NO CAPRICHO 😋👌🏼.
         DE 5 🖐🏼, DE 7 🖐🏼✌🏼, DE 10 🖐🏼🤚🏼 ?  ME DA UMA DE 5 🤚🏼.
-        AQUI, TÁ NA MÃO 👨🏼‍🍳🤝🍹.  TÁ AQUI 🍹. ISSO JAMES</h1>
+        AQUI, TÁ NA MÃO 👨🏼‍🍳🤝🍹.  TÁ AQUI 🍹. ISSO JAMES! 👍</h1>
 }
 
 export default Home;
