cf01c93b23ea1b565dcd5737d0ce01eabd1049c6
Guilherme <124900399+guiFrcs@users.noreply.github.com>
2026-06-23 23:54:27 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home() {
-    return <h1>Ô JÂIMESS 🗣🗣, EU QUERO UMA SALADA DE FRUATA 🍌🍏🍇🍓🤤.
-        OLHA QUE HABILIDADE 😏🤠🧐 OLHA QUE HABILIDADE 😏🤠🧐
-        EU QUERO UMA SALADA DE FRUTA, JAMES 😉🍏🍇.  NO CAPRICHO 😋👌🏼.
-        DE 5 🖐🏼, DE 7 🖐🏼✌🏼, DE 10 🖐🏼🤚🏼 ?  ME DA UMA DE 5 🤚🏼.
-        AQUI, TÁ NA MÃO 👨🏼‍🍳🤝🍹.  TÁ AQUI 🍹. ISSO JAMES! 👍</h1>
+  return (
+    <h1>
+      Ô JÂIMESS 🗣🗣, EU QUERO UMA SALADA DE FRUATA 🍌🍏🍇🍓🤤. OLHA QUE
+      HABILIDADE 😏🤠🧐 OLHA QUE HABILIDADE 😏🤠🧐 EU QUERO UMA SALADA DE FRUTA,
+      JAMES 😉🍏🍇. NO CAPRICHO 😋👌🏼. DE 5 🖐🏼, DE 7 🖐🏼✌🏼, DE 10 🖐🏼🤚🏼 ? ME DA UMA
+      DE 5 🤚🏼. AQUI, TÁ NA MÃO 👨🏼‍🍳🤝🍹. TÁ AQUI 🍹. ISSO JAMES! 👍
+    </h1>
+  );
 }
 
 export default Home;
